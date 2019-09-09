6bc8a1f647288407636c7f16ab9eac2cb3b19685
Great Ke <thegreatke@qq.com>
2019-09-09 17:52:27 +0800

    Initial commit

---
+++ src/main/java/com/nwpu/sign_up_system/SignUpSystemApplication.java
@@ -0,0 +1,15 @@
+package com.nwpu.sign_up_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableAutoConfiguration
+@SpringBootApplication
+public class SignUpSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignUpSystemApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/nwpu/sign_up_system/utils/SiteOwner.java
@@ -0,0 +1,12 @@
+package com.nwpu.sign_up_system.utils;
+
+/**
+ * @author: TheGreatKe
+ * @Date: 2018/7/27 14:27
+ * Describe: 文章信息
+ */
+public interface SiteOwner {
+
+    String SITE_OWNER = "西工大匿名水贴区版主——轲爷";
+
+}
